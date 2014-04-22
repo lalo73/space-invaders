8f0d31c5474042d39c3bd55ec18b860fc347972c
lalo <leandro.gz73@gmail.com>
2014-04-22 01:29:45 -0300

    Scene background is set when is set as current

--- space/src/main/java/scenes/SpaceScene.java
+++ space/src/main/java/scenes/SpaceScene.java
@@ -1,7 +1,10 @@
 package scenes;
 
+import com.uqbar.vainilla.GameComponent;
 import com.uqbar.vainilla.GameScene;
 import components.Ship;
+import resources.Resource;
+import space.SpaceGame;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +13,7 @@ import java.util.List;
 public class SpaceScene extends GameScene {
     List<Ship> shipList;
     List<Ship> invaderList;
+    GameComponent<SpaceScene> background;
 
     public SpaceScene() {
         super();
@@ -19,6 +23,12 @@ public class SpaceScene extends GameScene {
     private void init() {
         setInvaderList(new ArrayList<Ship>());
         setShipList(new ArrayList<Ship>());
+    }
+
+    @Override
+    public void onSetAsCurrent() {
+        super.onSetAsCurrent();
+        addBackground();
     }
 
     public List<Ship> getShipList() {
@@ -35,6 +45,20 @@ public class SpaceScene extends GameScene {
 
     public void setInvaderList(List<Ship> invaderList) {
         this.invaderList = invaderList;
+    }
+
+    public void addBackground() {
+        Resource resource = getGame().getResource("background");
+        this.background = new GameComponent<SpaceScene>(resource, 0, 0);
+        addComponents(getBackground());
+    }
+
+    public SpaceGame getGame() {
+        return (SpaceGame) super.getGame();
+    }
+
+    public GameComponent<SpaceScene> getBackground(){
+        return this.background;
     }
 
 
--- space/src/test/java/scenes/SpaceSceneTest.java
+++ space/src/test/java/scenes/SpaceSceneTest.java
@@ -3,20 +3,42 @@ package scenes;
 
 import components.Ship;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import space.SpaceGame;
 
 import java.util.ArrayList;
 
 public class SpaceSceneTest {
+
+    private SpaceScene scene;
+
+    @Before
+    public void setUp() {
+        this.scene = new SpaceScene();
+    }
+
     @Test
-    public void spaceSceneHasInvaderList(){
-        SpaceScene scene = new SpaceScene();
+    public void spaceSceneHasInvaderList() {
         Assert.assertEquals(scene.getInvaderList(), new ArrayList<Ship>());
     }
+
     @Test
-    public void spaceSceneHasShipList(){
-        SpaceScene scene = new SpaceScene();
+    public void spaceSceneHasShipList() {
         Assert.assertEquals(scene.getShipList(), new ArrayList<Ship>());
+    }
+
+    @Test
+    public void hasNoBackgroundOnInit() {
+        Assert.assertNull(this.scene.getBackground());
+    }
+
+    @Test
+    public void hasBackgroundAfterSetAsCurrent() {
+        SpaceGame game = new SpaceGame();
+        game.setCurrentScene(this.scene);
+
+        Assert.assertNotNull(this.scene.getBackground());
     }
 
 }
