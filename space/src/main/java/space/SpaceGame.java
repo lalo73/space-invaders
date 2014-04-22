defb2b4f88e6e1e1316396b2003b3c0c0fc70bc2
lalo <leandro.gz73@gmail.com>
2014-04-21 23:38:27 -0300

    Add space project

---
+++ space/src/main/java/space/SpaceGame.java
@@ -0,0 +1,28 @@
+package space;
+
+
+import com.uqbar.vainilla.Game;
+
+import java.awt.*;
+
+public class SpaceGame extends Game{
+    @Override
+    protected void initializeResources() {
+
+    }
+
+    @Override
+    protected void setUpScenes() {
+
+    }
+
+    @Override
+    public Dimension getDisplaySize() {
+        return null;
+    }
+
+    @Override
+    public String getTitle() {
+        return null;
+    }
+}
