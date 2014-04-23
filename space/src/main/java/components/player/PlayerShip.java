27185e974f4696b1e0476ce4c588eabc0df9c5f3
lalo <leandro.gz73@gmail.com>
2014-04-22 22:58:27 -0300

    Add PlayerShip component

---
+++ space/src/main/java/components/player/PlayerShip.java
@@ -0,0 +1,44 @@
+package components.player;
+
+
+import com.uqbar.vainilla.DeltaState;
+import com.uqbar.vainilla.appearances.Appearance;
+import com.uqbar.vainilla.events.constants.Key;
+import components.Ship;
+
+public class PlayerShip extends Ship {
+
+    private int defaultSpeed;
+
+    public PlayerShip(int x, int y, int xV, int yV, int speed) {
+        super(x, y, xV, yV, speed);
+        defaultSpeed = speed;
+    }
+
+    public PlayerShip(Appearance appearance, int x, int y, int xV, int yV, int speed) {
+        super(appearance, x, y, xV, yV, speed);
+        defaultSpeed = speed;
+    }
+
+    @Override
+    public void update(DeltaState deltaState) {
+        updateX(deltaState);
+        super.update(deltaState);
+    }
+
+    public void updateX(DeltaState deltaState) {
+        double speed = getSpeed();
+        setSpeed(0);
+        if (deltaState.isKeyBeingHold(Key.LEFT) && !deltaState.isKeyBeingHold(Key.RIGHT)) {
+            getUVector().setX(-1);
+            setSpeed(defaultSpeed);
+        }
+        if (deltaState.isKeyBeingHold(Key.RIGHT) && !deltaState.isKeyBeingHold(Key.LEFT)) {
+            getUVector().setX(1);
+            setSpeed(defaultSpeed);
+        }
+
+    }
+
+
+}
