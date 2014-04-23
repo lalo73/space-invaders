a6050ebbf5fbefaca192cb39443fb13792dd9cad
lalo <leandro.gz73@gmail.com>
2014-04-22 22:06:52 -0300

    Simplify StandardMove class

--- space/src/main/java/components/invaders/StandardMove.java
+++ space/src/main/java/components/invaders/StandardMove.java
@@ -4,7 +4,6 @@ import com.uqbar.vainilla.DeltaState;
 
 public class StandardMove extends InvaderMover {
     double initialPosition;
-    int direction = 1;
 
     public StandardMove(Invader invader) {
         super(invader);
@@ -14,28 +13,25 @@ public class StandardMove extends InvaderMover {
     @Override
     public void update(DeltaState deltaState) {
 
-        if(goingToRight()){
-            this.invader.setX(this.invader.getX() + distance(deltaState.getDelta()));
-        } else {
-            this.invader.setX(this.invader.getX() - distance(deltaState.getDelta()));
-        }
-
-        if((direction > 0 && this.invader.getX() > initialPosition + variance()) || (direction < 0 && this.invader.getX() < initialPosition - variance()) ){
-            direction = -direction;
+        if (goingToRight() && invader.getX() > (initialPosition + variance())) {
+            invader.getUVector().invertX();
+        } else if (goingToLeft() && invader.getX() < (initialPosition - variance())) {
+            invader.getUVector().invertX();
         }
 
     }
 
-    public boolean goingToRight(){
-        return direction == 1;
+    public boolean goingToRight() {
+        return invader.getUVector().getX() > 0;
     }
 
-    public double distance(double delta){
-        return 3*delta;
+    public boolean goingToLeft(){
+        return invader.getUVector().getX() < 0;
     }
 
-    public double variance(){
-        return 10;
+
+    public double variance() {
+        return 20;
     }
 
 }
