857cf65fd88cf72e1f6fdeccf058724720980728
lalo <leandro.gz73@gmail.com>
2014-04-22 22:07:06 -0300

    Create a row of invaders

--- space/src/main/java/scenes/levels/Level1.java
+++ space/src/main/java/scenes/levels/Level1.java
@@ -1,17 +1,16 @@
 package scenes.levels;
 
 
+import components.factories.InvaderFactory;
 import components.invaders.Invader;
-import pool.makers.InvaderMaker;
-import resources.Resource;
 import scenes.SpaceScene;
+
 
 public class Level1 extends SpaceScene {
     @Override
     public void addInvaders() {
-        Invader invader = getInvadersPool().getComponentMaker().createGreen();
-        invader.setX(20);
-        invader.setY(20);
-        this.addInvader(invader);
+        for(Invader invader : InvaderFactory.invadersRow(17, getGame(), 20, 20, 5, 10))
+            this.addInvader(invader);
+
     }
 }
